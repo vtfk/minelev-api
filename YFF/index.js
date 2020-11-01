daedbb5704b2d80a5c28a48c8e02a0213bf7ebda
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-01 17:59:16 +0100

    Refactors crud

--- YFF/index.js
+++ YFF/index.js
@@ -1,69 +1,36 @@
 const withTokenAuth = require('../lib/with-token-auth')
 const HTTPError = require('../lib/http-error')
 const getResponse = require('../lib/get-response-object')
-const { maal, utplasseringer, tilbakemeldinger, maalmaler } = require('../mock/yff')
+const { add, edit, get, remove } = require('../lib/yff-handler')
+
+function resolveAction (method) {
+  switch (method) {
+    case 'GET':
+      return get
+    case 'PUT':
+      return add
+    case 'POST':
+      return edit
+    case 'DELETE':
+      return remove
+  }
+}
 
 const handleYFF = async (context, req) => {
-  const { student, type, id } = req.params
+  const payload = req.params
+  const { student, type, id } = payload
   const { method } = req
   const user = req.token.upn
 
-  context.log(['handle-yff', 'student', student, 'user', user])
+  context.log(['handle-yff', 'method', method, 'student', student, 'user', user, 'type', type, 'id', `${id || 'alle'}`])
 
   try {
-    // GET: /:student/maal/:id
-    if (method === 'GET' && student && type === 'maal') {
-      context.log(['handle-yff', 'get-maal-for-student', 'student', student, 'user', user, 'id', `${id || 'alle'}`])
-      /*
-      const maal = await getMaal({
-        student,
-        user,
-        id
-      })
-      */
-      context.log(['handle-yff', 'get-maal-for-student', 'student', student, 'user', user, 'maal', maal.length])
-      return getResponse(maal)
-    }
-    // GET: /:student/utplasseringer/:id
-    if (method === 'GET' && student && type === 'utplasseringer') {
-      context.log(['handle-yff', 'get-utplasseringer-for-student', 'student', student, 'user', user, 'id', `${id || 'alle'}`])
-      /*
-      const utplasseringer = await getUtplasseringer({
-        student,
-        user,
-        id
-      })
-      */
-      context.log(['handle-yff', 'get-utplasseringer-for-student', 'student', student, 'user', user, 'utplasseringer', utplasseringer.length])
-      return getResponse(utplasseringer)
-    }
-    // GET: /:student/tilbakemeldinger
-    if (method === 'GET' && student && type === 'tilbakemeldinger') {
-      context.log(['handle-yff', 'get-tilbakemeldinger-for-student', 'student', student, 'user', user, 'id', `${id || 'alle'}`])
-      /*
-      const tilbakemeldinger = await getTilbakemeldinger({
-        student,
-        user,
-        id
-      })
-      */
-      context.log(['handle-yff', 'get-tilbakemeldinger-for-student', 'student', student, 'user', user, 'tilbakemeldinger', tilbakemeldinger.length])
-      return getResponse(tilbakemeldinger)
-    }
-    // GET: /:user/maalmaler
-    if (method === 'GET' && student && type === 'maalmaler') {
-      context.log(['handle-yff', 'get-maalmaler', 'user', user, 'id', `${id || 'alle'}`])
-      /*
-      const maler = await getMaler({
-        user,
-        id
-      })
-      */
-      context.log(['handle-yff', 'get-maalmaler', 'user', user, 'tilbakemeldinger', maalmaler.length])
-      return getResponse(maalmaler)
-    }
+    const action = resolveAction(method)
+    const result = action(payload)
+    context.log(['handle-yff', 'method', method, 'student', student, 'user', user, 'type', type, 'id', `${id || 'alle'}`, 'result', result.length])
+    return getResponse(result)
   } catch (error) {
-    context.log.error(['handle-yff', 'student', student, 'user', user, 'id', id, 'err', error.message])
+    context.log.error(['handle-yff', 'method', method, 'student', student, 'user', user, 'id', `${id || 'alle'}`, 'err', error.message])
     if (error instanceof HTTPError) return error.toJSON()
     return new HTTPError(500, 'An unknown error occured', error)
   }
