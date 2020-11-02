98354052189d8a55b0a6d6307aca776e76da8aae
Mats Andreassen <matsand@live.no>
2020-11-02 18:59:44 +0100

    Replaced dependency azure-jwt-verify with azure-ad-verify-token (closes issue #5)

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const CLIENT_ID = process.env.CLIENT_ID
 
 module.exports = {
   TOKEN_AUTH: {
-    JWK_URI: process.env.TOKEN_AUTH_JWK_URI || `https://login.microsoftonline.com/${TENANT_ID}/discovery/v2.0/keys`,
-    ISS: process.env.TOKEN_AUTH_ISS || undefined,
-    AUD: process.env.TOKEN_AUTH_AUD || CLIENT_ID || undefined // Application Client ID
+    jwksUri: process.env.TOKEN_AUTH_JWK_URI || `https://login.microsoftonline.com/${TENANT_ID}/discovery/v2.0/keys`,
+    issuer: process.env.TOKEN_AUTH_ISS || undefined,
+    audience: process.env.TOKEN_AUTH_AUD || CLIENT_ID || undefined // Application Client ID
   },
   DEMO: process.env.DEMO === 'true',
   DEMO_USER: process.env.DEMO_USER,
