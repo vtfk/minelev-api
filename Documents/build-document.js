bf33beebede57acd026d2721381a3f3e55170131
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-02 19:13:53 +0100

    Legger inn flere skjelett

--- Documents/build-document.js
+++ Documents/build-document.js
@@ -7,16 +7,28 @@ module.exports = async payload => {
     delete payload.id
     return payload
   }
-  if (method === 'POST' && type === 'utplassering-bekreftelse') {
-    // må hente info om elev og utplassering
+  if (method === 'POST' && type === 'yff-utplassering-bekreftelse') {
+    // TODO: må hente info om elev og utplassering
     return payload
   }
-  if (method === 'POST' && type === 'lokal-laereplan') {
-    // må hente info om elev, maal og utplasseringer
+  if (method === 'POST' && type === 'yff-lokal-laereplan') {
+    // TODO: må hente info om elev, maal og utplasseringer
     return payload
   }
-  if (method === 'POST' && type === 'utplassering-tilbakemelding') {
-    // må hente info om elev, utplassering, maal og tilbakemelding
+  if (method === 'POST' && type === 'yff-utplassering-tilbakemelding') {
+    // TODO: må hente info om elev, utplassering, maal og tilbakemelding
+    return payload
+  }
+  if (method === 'POST' && type === 'elev-varsel') {
+    // TODO: må hente info om elev
+    return payload
+  }
+  if (method === 'POST' && type === 'elev-notat') {
+    // TODO: må hente info om elev
+    return payload
+  }
+  if (method === 'POST' && type === 'elev-samtale') {
+    // TODO: må hente info om elev
     return payload
   }
 }
