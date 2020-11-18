aaceba18789f9739b403874987cf6fa1fa9fa454
Mats Andreassen <matsand@live.no>
2020-11-18 17:02:43 +0100

    Refactoring document methods

--- Documents/build-document-query.js
+++ Documents/build-document-query.js
@@ -1,128 +1,51 @@
 const { logger } = require('@vtfk/logger')
 const { ObjectId } = require('mongodb')
-const { getMyUser, getMyStudents } = require('../lib/get-pifu-data')
-const Ajv = require('ajv')
-const config = require('../config')
+const { encryptContent } = require('../lib/encryption')
 const HTTPError = require('../lib/http-error')
-const validateTeacherStudentRelation = require('../lib/validate-teacher-student-relation')
+const repackDocumentSchool = require('../lib/repack-document-school')
 const repackDocumentStudent = require('../lib/repack-document-student')
 const repackDocumentTeacher = require('../lib/repack-document-teacher')
-const repackAjvError = require('../lib/repack-ajv-error')
-const documentSchema = require('../models/document.json')
-const repackDocumentSchool = require('../lib/repack-document-school')
-const { encryptContent } = require('../lib/encryption')
+const config = require('../config')
 
-const ajv = new Ajv({ allErrors: true, jsonPointers: true })
-const validate = ajv.compile(documentSchema)
+module.exports.getStudentDocumentsQuery = (students, type, id) => {
+  const query = {}
 
-module.exports = async payload => {
-  const { method, id, type, student, user, body } = payload
-  const logPrefix = ['build-document', 'user', user, 'student', student || 'all', method]
+  const studentUsernames = students.map(student => student.username || student.userName)
+  query['student.username'] = { $in: studentUsernames }
 
-  // Get document with specified student, id and/or type
-  if (method === 'GET') {
-    payload = {}
-    if (type) payload.type = type
-    if (id) {
-      try {
-        payload._id = new ObjectId(id)
-      } catch (error) {
-        throw new HTTPError(400, `The provided id is invalid: ${error.message}`)
-      }
+  if (type) query.type = type
+  if (id) {
+    try {
+      query._id = new ObjectId(id)
+    } catch (error) {
+      throw new HTTPError(400, `The provided id is invalid: ${error.message}`)
     }
-
-    // Get specific student, or if no specific student was passed, get documents for all my students
-    if (student) {
-      const studentObj = await validateTeacherStudentRelation(user, student)
-      payload['student.username'] = studentObj.userName
-    } else {
-      const myStudents = await getMyStudents(user)
-      const myStudentsUsernames = myStudents.map(student => student.userName)
-      payload['student.username'] = { $in: myStudentsUsernames }
-    }
-
-    logger('verbose', [...logPrefix, payload])
-    return payload
   }
 
-  // If some other method than POST from this point, return an error
-  if (method !== 'POST') throw new HTTPError(405, `Method ${method} is not allowed for this endpoint!`)
-
-  // Verify that we got a body
-  if (!body) throw new HTTPError(400, 'No body was provided!', { method, student, type, id })
-
-  // Append student to body if provided in query param and verify that the values match
-  if (student && !body.student) body.student = { username: student }
-  if (student && student !== body.student.username) throw new HTTPError(400, 'The student provided in query and body doesn\'t match!', { query: student, body: body.student.username })
-
-  // Validate that the posted data matches the schema
-  const valid = validate(body)
-  if (!valid) {
-    logger('warn', [...logPrefix, 'schema validation failed', validate.errors.map(repackAjvError)])
-    throw new HTTPError(400, 'Invalid request body', { summary: validate.errors.map(repackAjvError), schemaErrors: validate.errors })
-  }
-
-  // Get current user / teacher thats making the request
-  const teacherObj = await getMyUser(user)
-  if (!teacherObj) {
-    logger('error', [...logPrefix, 'no teacher found for current user'])
-    throw new HTTPError(403, 'No teacher found', { user })
-  }
-
-  body.teacher = repackDocumentTeacher(teacherObj)
-  body.user = body.teacher.username || teacherObj.userName
-
-  // Make sure that the teacher have access to the student
-  const studentObj = await validateTeacherStudentRelation(user, body.student.username)
-  if (!studentObj) {
-    logger('error', [...logPrefix, 'no student found'])
-    throw new HTTPError(403, 'No student found', { student: body.student.username })
-  }
-
-  body.student = repackDocumentStudent(studentObj)
-
-  // Get school from student props
-  body.school = repackDocumentSchool(studentObj)
-
-  body.isEncrypted = body.isEncrypted || false
-
-  // Encrypt content if type should be encrypted and it isn't already
-  if (!body.isEncrypted && config.ENCRYPTED_DOCUMENT_TYPES.includes(body.type)) {
-    logger('info', '')
-    body.content = encryptContent(body.content)
-    body.isEncrypted = true
-  }
-
-  // Queue and add queued status
-  body.isQueued = true
-  body.status = [{ status: 'queued', timestamp: new Date().getTime() }]
-
-  return body
+  return query
 }
 
-/*
-  if (method === 'POST' && type === 'yff-utplassering-bekreftelse') {
-    // TODO: må hente info om elev og utplassering
-    return payload
+module.exports.getNewDocumentQuery = ({ user, body, student, teacher }) => {
+  const query = { ...body }
+
+  query.user = user // User is used to set created + last modified date, but is removed before insertion
+  query.student = repackDocumentStudent(student)
+  query.teacher = repackDocumentTeacher(teacher)
+  query.school = repackDocumentSchool(student)
+  query.isEncrypted = body.isEncrypted || false
+
+  // TODO: Append additional type date (mostly yff data)
+
+  // Encrypt content if type should be encrypted and it isn't already
+  if (!query.isEncrypted && config.ENCRYPTED_DOCUMENT_TYPES.includes(query.type)) {
+    logger('verbose', ['new-document-query', 'type', query.type, 'content of this type should be encypted'])
+    query.content = encryptContent(query.content)
+    query.isEncrypted = true
   }
-  if (method === 'POST' && type === 'yff-lokal-laereplan') {
-    // TODO: må hente info om elev, maal og utplasseringer
-    return payload
-  }
-  if (method === 'POST' && type === 'yff-utplassering-tilbakemelding') {
-    // TODO: må hente info om elev, utplassering, maal og tilbakemelding
-    return payload
-  }
-  if (method === 'POST' && type === 'varsel') {
-    // TODO: må hente info om elev
-    return payload
-  }
-  if (method === 'POST' && type === 'notat') {
-    // TODO: må hente info om elev
-    return payload
-  }
-  if (method === 'POST' && type === 'samtale') {
-    // TODO: må hente info om elev
-    return payload
-  }
-*/
+
+  // Queue and add status array with initial status
+  query.status = [{ status: 'queued', timestamp: new Date().getTime() }]
+  query.isQueued = true
+
+  return query
+}
--- Documents/index.js
+++ Documents/index.js
@@ -1,13 +1,18 @@
 const { logger } = require('@vtfk/logger')
 const { add, get } = require('../lib/crud')
+const { getMyStudents, getMyUser } = require('../lib/get-pifu-data')
+const { getStudentDocumentsQuery, getNewDocumentQuery } = require('./build-document-query')
 const withTokenAuth = require('../lib/with-token-auth')
-const HTTPError = require('../lib/http-error')
 const getResponse = require('../lib/get-response-object')
-const buildDocument = require('./build-document-query')
+const HTTPError = require('../lib/http-error')
+const validateTeacherStudentRelation = require('../lib/validate-teacher-student-relation')
+const validateDocumentSchema = require('../lib/validate-document-schema')
+const buildDocumentQuery = require('./build-document-query')
 const config = require('../config')
 
+const collection = config.MONGODB_COLLECTION_DOCUMENTS
+
 function resolveAction (method) {
-  const collection = config.MONGODB_COLLECTION_DOCUMENTS
   switch (method) {
     case 'GET':
       return (params) => get(collection, params)
@@ -17,28 +22,70 @@ function resolveAction (method) {
 }
 
 const handleDocuments = async (context, req) => {
-  const payload = req.params
-  const { student, type, id } = payload
+  const { student, type, id } = req.params
   const { method, token, body } = req
   const user = token.upn
-  payload.user = user
-  payload._id = id
-  payload.method = method
-  payload.body = body
-
-  logger('info', ['handle-documents', 'method', method, 'user', user, 'student', `${student || 'alle'}`, 'type', `${type || 'alle'}`, 'id', `${id || 'alle'}`])
 
   try {
-    // Create document query
-    const document = await buildDocument(payload)
+    // Get current user
+    logger('info', ['handle-documents', 'user', user, 'get-user'])
+    const teacherObj = await getMyUser(user)
+    logger('info', ['handle-documents', 'user', user, 'get-user', 'type', teacherObj.type])
 
-    // Resolve action and run query
-    const action = resolveAction(method)
-    const result = await action(document)
-    logger('info', ['handle-documents', 'method', method, 'student', student, 'user', user, 'type', type, 'id', `${id || 'alle'}`, 'result', result.length])
-    return getResponse(result)
+    // Get all my students
+    logger('info', ['handle-documents', 'user', user, 'get-students'])
+    const students = await getMyStudents(user)
+    logger('info', ['handle-documents', 'user', user, 'get-students', students.length, 'students'])
+
+    // Get student and verify relation before continuing
+    const studentObj = student ? await validateTeacherStudentRelation(user, student, students) : null
+    logger('info', ['handle-documents', 'user', user, 'student', studentObj ? 'student related to teacher' : 'no specific student, returning documents for all'])
+
+    // GET: /documents/?student/?type/?id
+    if (method === 'GET') {
+      logger('info', ['handle-documents', 'user', user, 'get-documents', 'build query'])
+      const query = getStudentDocumentsQuery(studentObj ? [studentObj] : students, type, id)
+
+      logger('info', ['handle-documents', 'user', user, 'get-documents', 'get documents'])
+      const documents = await get(collection, query)
+      logger('info', ['handle-documents', 'user', user, 'get-documents', `got ${documents.length} documents`])
+
+      return getResponse(documents)
+    }
+
+    // POST: /documents/?student
+    if (method === 'POST') {
+      logger('info', ['handle-documents', 'user', user, 'new-document'])
+
+      // Verify that we got a body
+      if (!body) throw new HTTPError(400, 'No body was provided!', { method, student, type, id })
+
+      // Append student to body if provided in query param and verify that the values match
+      if (student && !body.student) body.student = { username: student }
+      if (student && student !== body.student.username) throw new HTTPError(400, 'The student provided in query and body doesn\'t match!', { query: student, body: body.student.username })
+
+      // Get the student from body if we don't have it already
+      const bodyStudentObj = studentObj || await validateTeacherStudentRelation(user, body.student.username, students)
+
+      // Validate that the posted data matches the schema
+      const valid = validateDocumentSchema(body)
+      logger('info', ['handle-documents', 'user', user, 'new-document', 'schema validated', valid ? 'successful' : 'unsuccessful'])
+
+      // Build document query
+      logger('info', ['handle-documents', 'user', user, 'new-document', 'build query'])
+      const newDocumentQuery = getNewDocumentQuery({ user, body, teacher: teacherObj, student: bodyStudentObj })
+
+      logger('info', ['handle-documents', 'user', user, 'new-documents', 'create document'])
+      const newDocument = await add(collection, newDocumentQuery)
+      logger('info', ['handle-documents', 'user', user, 'new-documents', 'created document with _id', newDocument._id])
+
+      return getResponse(newDocument)
+    }
+
+    // If some other method from this point, return an error as this shouldn't happen
+    if (method !== 'POST') throw new HTTPError(405, `Method ${method} is not allowed for this endpoint!`)
   } catch (error) {
-    logger('error', ['handle-documents', 'method', method, 'student', student, 'user', user, 'id', `${id || 'alle'}`, 'err', error.message])
+    logger('error', ['handle-documents', 'method', method, 'user', user, 'student', `${student || 'alle'}`, 'type', `${type || 'alle'}`, 'id', `${id || 'alle'}`, 'err', error.message])
     if (error instanceof HTTPError) return error.toJSON()
     return new HTTPError(500, 'An unknown error occured', error)
   }
