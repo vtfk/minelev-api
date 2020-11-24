49b50c0f7321310f5fd665e7a4cdea29900124df
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-24 16:59:51 +0100

    Legger til rammene for brreg-oppslag

---
+++ Brreg/YFF/index.js
@@ -0,0 +1,28 @@
+const brreg = require('brreg')
+const withTokenAuth = require('../lib/with-token-auth')
+const HTTPError = require('../lib/http-error')
+const getResponse = require('../lib/get-response-object')
+const { logger } = require('@vtfk/logger')
+
+const handleBrreg = async (context, req) => {
+  const { query } = req.params
+  const user = req.token.upn
+  const options = {
+    query: query
+  }
+
+  logger('info', ['handle-brreg', 'query', query, 'user', user])
+
+  try {
+    const data = await brreg(options)
+    const results = [...data.enhetsregisteret.data.entries, ...data.underenheter.data.entries]
+    logger('info', ['handle-brreg', 'query', query, 'user', user, 'result', results.length])
+    return getResponse(results)
+  } catch (error) {
+    logger('error', ['handle-brreg', 'query', query, 'user', user, 'error', error.message])
+    if (error instanceof HTTPError) return error.toJSON()
+    return new HTTPError(500, 'An unknown error occured', error)
+  }
+}
+
+module.exports = (context, request) => withTokenAuth(context, request, handleBrreg)
